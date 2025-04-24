f27f1192a3a085f497739ceceea9fd790f791649
Alan Reyes Luis <alannreyes02@gmail.com>
2025-04-23 19:08:05 -0600

    creacion de tablas de fabricante y producto

--- DB tienda.sql
+++ DB tienda.sql
@@ -1,2 +1,15 @@
 create database tienda;
 use tienda;
+
+create table fabricante(
+id int auto_increment primary key,
+nombre varchar (100)
+);
+
+create table producto (
+id int auto_increment primary key,
+nombre varchar (100),
+precio decimal (10,2),
+id_fabricante int,
+foreign key (id_fabricante) references fabricante (id)
+);
